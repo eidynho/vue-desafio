fbd235d961cc30eecaaaee82a5d791503dada220
eidynho <zviniciuseidy2@gmail.com>
2024-08-04 18:44:38 -0300

    feat: base of side bar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import Divider from 'primevue/divider'
+
+import { useItemsStore } from '@/stores/items'
+
+const itemsStore = useItemsStore()
+</script>
+
+<template>
+  <div class="border-r w-80 p-4 mr-4 border-r-neutral-800 min-h-dvh">
+    <div class="flex flex-col gap-2">
+      <Button
+        icon="pi pi-search"
+        label="Nova busca"
+        severity="success"
+        text
+        class="w-full !justify-start !text-sm"
+      />
+      <Button
+        icon="pi pi-trash"
+        label="Limpar buscas recentes"
+        severity="danger"
+        text
+        class="w-full !justify-start !text-sm"
+      />
+    </div>
+
+    <Divider />
+
+    <span class="block mb-3 text-sm px-3 text-gray-400">Buscas recentes</span>
+
+    <template v-if="!itemsStore.fetchedItems.length">
+        <span class="text-sm px-3">Nenhuma busca recente.</span>
+    </template>
+    <template v-else v-for="fetchedItem in itemsStore.fetchedItems" :key="fetchedItem.query">
+      <Button
+        :label="fetchedItem.query"
+        severity="secondary"
+        text
+        class="w-full mb-2 !justify-start !text-sm !text-white"
+      />
+    </template>
+  </div>
+</template>
